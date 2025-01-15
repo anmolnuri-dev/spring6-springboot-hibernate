b8fb0c661d39dac24007147b5635de230d2fdfc6
anmol nuri <anmol.nuri10@gmail.com>
2025-01-15 12:31:01 -0500

    first commit

---
+++ section1/01-spring-boot-demo/src/main/java/com/anmol/udemy/rest/FunRestController.java
@@ -0,0 +1,14 @@
+package com.anmol.udemy.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+    //expose "/" endpoint that returns "Hello World"
+    @GetMapping("/")
+    public String sayHello(){
+        return "Hello, World!";
+    }
+
+}
